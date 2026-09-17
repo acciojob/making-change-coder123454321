254b8e4302d57a9c9d44d0d33ce1348576890480
coder123454321 <115930973+coder123454321@users.noreply.github.com>
2026-09-17 23:13:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const makeChange = (c) => {
   // your name here
+	let q=Math.flor(c/25);
+	c=c%25;
+	let d=Math.flor(c/10);
+	c=c%10;
+	let n=Math.flor(c/5);
+	c=c%5;
+	let p=c;
+	return {
+		"q"=q,
+		"d"=d,
+		"n"=n,
+		"p"=p
+	};
 };
-
 // Do not the change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
